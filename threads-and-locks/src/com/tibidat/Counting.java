d4fdbb0235ecff7e0b26f972cf1ed02e22ce2c3f
Moritz <moritz@tibidat.com>
2014-10-27 19:23:11 +0100

    Refactored Counting to be use an atomic int instead of locking.

--- threads-and-locks/src/com/tibidat/Counting.java
+++ threads-and-locks/src/com/tibidat/Counting.java
@@ -1,33 +1,25 @@
 package com.tibidat;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counting {
     public static void main(String[] args) throws InterruptedException {
-        class Counter {
-            private int count = 0;
+        final AtomicInteger counter = new AtomicInteger();
 
-            public synchronized void increment() {
-                ++count;
-            }
-
-            public int getCount() {
-                return count;
-            }
-        }
-        final Counter counter = new Counter();
         class CountingThread extends Thread {
             public void run() {
                 for (int x = 0; x < 10000; ++x) {
-                    counter.increment();
+                    counter.incrementAndGet();
                 }
             }
         }
-
         CountingThread t1 = new CountingThread();
         CountingThread t2 = new CountingThread();
+
         t1.start();
         t2.start();
         t1.join();
         t2.join();
-        System.out.println(counter.getCount());
+        System.out.println(counter.get());
     }
 }
