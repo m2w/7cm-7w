78a3a9faf90f3010d3ea9193f7bb572dc92d4bac
Moritz <moritz@tibidat.com>
2014-10-27 18:59:56 +0100

    Refactored the Philosopher to also use Reentrant locking.

--- threads-and-locks/src/com/tibidat/Philosopher.java
+++ threads-and-locks/src/com/tibidat/Philosopher.java
@@ -1,19 +1,16 @@
 package com.tibidat;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Philosopher extends Thread {
-    private Chopstick first, second;
+    private ReentrantLock leftChopstick, rightChopstick;
     private Random random;
 
-    public Philosopher(Chopstick left, Chopstick right) {
-        if (left.getId() < right.getId()) {
-            first = left;
-            second = right;
-        } else {
-            first = right;
-            second = left;
-        }
+    public Philosopher(ReentrantLock left, ReentrantLock right) {
+        leftChopstick = left;
+        rightChopstick = right;
         random = new Random();
     }
 
@@ -21,10 +18,17 @@ public class Philosopher extends Thread {
         try {
             while (true) {
                 Thread.sleep(random.nextInt(1000));
-                synchronized (first) {
-                    synchronized (second) {
-                        Thread.sleep(random.nextInt(1000));
+                leftChopstick.lock();
+                try {
+                    if (rightChopstick.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                        try {
+                            Thread.sleep(random.nextInt(1000));
+                        } finally {
+                            rightChopstick.unlock();
+                        }
                     }
+                } finally {
+                    leftChopstick.unlock();
                 }
             }
         } catch (InterruptedException e) {
