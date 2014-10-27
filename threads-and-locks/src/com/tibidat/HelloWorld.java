9d4e7581e49f5ba25d3d226f8847b8c5455d8a9a
Moritz <moritz@tibidat.com>
2014-10-27 17:30:34 +0100

    Added the HelloWorld example.

---
+++ threads-and-locks/src/com/tibidat/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.tibidat;
+
+public class HelloWorld {
+    public static void main(String[] args) throws InterruptedException {
+        Thread myThread = new Thread() {
+            public void run() {
+                System.out.println("Hello from new thread");
+            }
+        };
+        myThread.start();
+        Thread.yield();
+        System.out.println("Hello from the main thread");
+        myThread.join();
+    }
+}
