b3ed78ad4b5bb2644a0edafab863d290068cb0d6
Moritz <moritz@tibidat.com>
2014-11-02 13:28:42 +0100

    Added a parallel implementation of frequencies.

--- functional-programming/src/wordcount/counter.clj
+++ functional-programming/src/wordcount/counter.clj
@@ -1,4 +1,5 @@
-(ns wordcount.counter)
+(ns wordcount.counter
+  (:require [wordcount.reducer :as r]))
 
 (defn- get-words-re [text]
   (re-seq #"\w+" text))
@@ -16,3 +17,9 @@
 (defn count-words-partitioned [pages]
   (reduce (partial merge-with +)
           (pmap count-words-sequential (partition-all 100 pages))))
+
+(defn parallel-frequencies [coll]
+  (r/my-fold
+   (partial merge-with +)
+   (fn [counts x] (assoc counts x (inc (get counts x 0))))
+   coll))
