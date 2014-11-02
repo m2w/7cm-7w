09c8cf31a49c6eb28ff424b93392cca7fe82b067
Moritz <moritz@tibidat.com>
2014-11-02 13:23:53 +0100

    Added custom map and fold implementations.
    Also cleaned up the counter code a little.

--- functional-programming/src/wordcount/counter.clj
+++ functional-programming/src/wordcount/counter.clj
@@ -7,8 +7,12 @@
   (get-words-re (:text page)))
 
 (defn count-words-sequential [pages]
-  (frequencies (mapcat #(get-words (:text %)) pages)))
+  (frequencies (mapcat get-words pages)))
 
 (defn count-words-parallel [pages]
   (reduce (partial merge-with +)
           (pmap #(frequencies (get-words %)) pages)))
+
+(defn count-words-partitioned [pages]
+  (reduce (partial merge-with +)
+          (pmap count-words-sequential (partition-all 100 pages))))
---
+++ functional-programming/src/wordcount/reducer.clj
@@ -0,0 +1,32 @@
+(ns wordcount.reducer
+  (:require [clojure.core.protocols :refer [CollReduce coll-reduce]]
+            [clojure.core.reducers :refer [CollFold coll-fold]]))
+
+(defn my-reduce
+  ([f coll] (coll-reduce coll f))
+  ([f init coll] (coll-reduce coll f init)))
+
+(defn make-reducer [reducible transformf]
+  (reify
+    CollFold
+    (coll-fold [_ n combinef reducef]
+      (coll-fold reducible n combinef (transformf reducef)))
+    CollReduce
+    (coll-reduce [_ f1]
+      (coll-reduce reducible (transformf f1) (f1)))
+    (coll-reduce [_ f1 init]
+      (coll-reduce reducible (transformf f1) init))))
+
+(defn my-map [mapf reducible]
+  (make-reducer reducible
+                (fn [reducef]
+                  (fn [acc v]
+                    (reducef acc (mapf v))))))
+
+(defn my-fold
+  ([reducef coll]
+     (my-fold reducef reducef coll))
+  ([combinef reducef coll]
+     (my-fold 512 combinef reducef coll))
+  ([n combinef reducef coll]
+     (coll-fold coll n combinef reducef)))
